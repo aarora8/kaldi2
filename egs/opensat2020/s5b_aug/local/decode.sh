08b419cd5acb6ed9d9cfb8c1c80fafaa22747bea
aarora8 <aarora8@jhu.edu>
2020-06-25 19:57:21 -0400

    adding initial vad changes and extract noises get duration

--- egs/opensat2020/s5b_aug/local/decode.sh
+++ egs/opensat2020/s5b_aug/local/decode.sh
@@ -1,110 +1,83 @@
 #!/usr/bin/env bash
-
-# Copyright 2016 Johns Hopkins University (Author: Daniel Povey, Vijayaditya Peddinti)
-#           2019 Vimal Manohar 
-# Apache 2.0.
-
-# This script does 2-stage decoding where the first stage is used to get 
-# reliable frames for i-vector extraction.
-
-set -e
-
-# general opts
+# Begin configuration section.
+nj=8
 stage=0
-nj=20
+sad_stage=0
+score_sad=true
+diarizer_stage=0
+decode_diarize_stage=0
+score_stage=0
 
-# ivector opts
-max_count=75  # parameter for extract_ivectors.sh
-sub_speaker_frames=6000
-get_weights_from_ctm=true
-weights_file=   # use weights from this archive (must be compressed using gunzip)
-silence_weight=0.00001   # apply this weight to silence frames during i-vector extraction
-ivector_dir=exp/nnet3_org_1d
-graph_affix=
-score_opts="--min-lmwt 6 --max-lmwt 13"
+enhancement=beamformit
+
+# training data
+train_set=train_worn_simu_u400k
+test_sets="dev_${enhancement}_dereverb eval_${enhancement}_dereverb"
+
+. ./utils/parse_options.sh
 
 . ./cmd.sh
-[ -f ./path.sh ] && . ./path.sh
-. utils/parse_options.sh || exit 1;
+. ./path.sh
+. ./conf/sad.conf
 
-if [ $# -ne 4 ]; then
-  echo "Usage: $0 [options] <data-dir> <lang-dir> <graph-dir> <model-dir>"
-  echo " Options:"
-  echo "    --stage (0|1|2)   # start scoring script from part-way through."
-  echo "e.g.:"
-  echo "$0 data/dev data/lang exp/tri5a/graph_pp exp/nnet3/tdnn"
-  exit 1;
-fi
+# This script also needs the phonetisaurus g2p, srilm, beamformit
+./local/check_tools.sh || exit 1
 
-data=$1 # data directory 
-lang=$2 # data/lang
-graph=$3 #exp/tri5a/graph_pp
-dir=$4 # exp/nnet3/tdnn
 
-model_affix=`basename $dir`
-data_set=$(basename $data)
-
-if [ $stage -le 1 ]; then
-  echo "Extracting i-vectors, stage 1"
-  steps/online/nnet2/extract_ivectors_online.sh --cmd "$train_cmd" --nj 20 \
-    --max-count $max_count \
-    ${data}_hires $ivector_dir/extractor \
-    $ivector_dir/ivectors_${data_set}_stage1;
-
-  ivector_scale_affix=_scale0.75
-  echo "$0: Scaling iVectors, stage 1"
-  srcdir=$ivector_dir/ivectors_${data_set}_stage1
-  outdir=$ivector_dir/ivectors_${data_set}${ivector_scale_affix}_stage1
-  mkdir -p $outdir
-  $train_cmd $outdir/log/scale_ivectors.log \
-    copy-matrix --scale=0.75 scp:$srcdir/ivector_online.scp ark:- \| \
-    copy-feats --compress=true ark:-  ark,scp:$outdir/ivector_online.ark,$outdir/ivector_online.scp;
-  cp $srcdir/ivector_period $outdir/ivector_period
-fi
-
-decode_dir=$dir/decode_${data_set}${affix}
-if [ $stage -le 2 ]; then
-  echo "Generating lattices, stage 1"
-  steps/nnet3/decode.sh --nj $nj --cmd "$decode_cmd" \
-    --acwt 1.0 --post-decode-acwt 10.0 \
-    --online-ivector-dir $ivector_dir/ivectors_${data_set}${ivector_scale_affix}_stage1 \
-    $graph ${data}_hires ${decode_dir}_stage1;
-fi
+#######################################################################
+# Perform SAD on the dev/eval data
+#######################################################################
+dir=exp/segmentation${affix}
+sad_work_dir=exp/sad${affix}_${nnet_type}/
+sad_nnet_dir=$dir/tdnn_${nnet_type}_sad_1a
 
 if [ $stage -le 3 ]; then
-  if $get_weights_from_ctm; then
-    if [ ! -z $weights_file ]; then
-      echo "$0: Using provided vad weights file $weights_file"
-      ivector_extractor_weights=$weights_file
-    else
-      echo "$0 : Generating vad weights file"
-      ivector_extractor_weights=${decode_dir}_stage1/weights${affix}.gz
-      local/extract_vad_weights.sh --silence-weight $silence_weight \
-        --cmd "$decode_cmd" ${iter:+--iter $iter} \
-        ${data}_hires $lang \
-        ${decode_dir}_stage1 $ivector_extractor_weights
+  for datadir in ${test_sets}; do
+    test_set=data/${datadir}
+    if [ ! -f ${test_set}/wav.scp ]; then
+      echo "$0: Not performing SAD on ${test_set}"
+      exit 0
     fi
-  else
-    # get weights from best path decoding
-    ivector_extractor_weights=${decode_dir}_stage1
-  fi
+    # Perform segmentation
+    local/segmentation/detect_speech_activity.sh --nj $nj --stage $sad_stage \
+      $test_set $sad_nnet_dir mfcc $sad_work_dir \
+      data/${datadir} || exit 1
+
+    test_dir=data/${datadir}_${nnet_type}_seg
+    mv data/${datadir}_seg ${test_dir}/
+    cp data/${datadir}/{segments.bak,utt2spk.bak} ${test_dir}/
+    # Generate RTTM file from segmentation performed by SAD. This can
+    # be used to evaluate the performance of the SAD as an intermediate
+    # step.
+    steps/segmentation/convert_utt2spk_and_segments_to_rttm.py \
+      ${test_dir}/utt2spk ${test_dir}/segments ${test_dir}/rttm
+
+    if [ $score_sad == "true" ]; then
+      echo "Scoring $datadir.."
+      # We first generate the reference RTTM from the backed up utt2spk and segments
+      # files.
+      ref_rttm=${test_dir}/ref_rttm
+      steps/segmentation/convert_utt2spk_and_segments_to_rttm.py ${test_dir}/utt2spk.bak \
+        ${test_dir}/segments.bak ${test_dir}/ref_rttm
+
+      # To score, we select just U06 segments from the hypothesis RTTM.
+      hyp_rttm=${test_dir}/rttm.U06
+      grep 'U06' ${test_dir}/rttm > ${test_dir}/rttm.U06
+      echo "Array U06 selected for scoring.."
+      
+      if $use_new_rttm_reference == "true"; then
+        echo "Use the new RTTM reference."
+        mode="$(cut -d'_' -f1 <<<"$datadir")"
+        ref_rttm=./chime6_rttm/${mode}_rttm
+      fi
+
+      sed 's/_U0[1-6].ENH//g' $ref_rttm > $ref_rttm.scoring
+      sed 's/_U0[1-6].ENH//g' $hyp_rttm > $hyp_rttm.scoring
+      cat ./local/uem_file | grep 'U06' | sed 's/_U0[1-6]//g' > ./local/uem_file.tmp
+      md-eval.pl -1 -c 0.25 -u ./local/uem_file.tmp -r $ref_rttm.scoring -s $hyp_rttm.scoring |\
+        awk 'or(/MISSED SPEECH/,/FALARM SPEECH/)'
+    fi
+  done
 fi
 
-if [ $stage -le 4 ]; then
-  echo "Extracting i-vectors, stage 2 with weights from $ivector_extractor_weights"
-  steps/online/nnet2/extract_ivectors.sh --cmd "$train_cmd" --nj 20 \
-    --silence-weight $silence_weight \
-    --sub-speaker-frames $sub_speaker_frames --max-count $max_count \
-    ${data}_hires $lang $ivector_dir/extractor \
-    $ivector_extractor_weights $ivector_dir/ivectors_${data_set};
-fi
-
-if [ $stage -le 5 ]; then
-  echo "Generating lattices, stage 2 with --acwt $acwt"
-  rm -f ${decode_dir}/.error
-  steps/nnet3/decode.sh --nj $nj --cmd "$decode_cmd" \
-      --acwt 1.0 --post-decode-acwt 10.0 \
-      --online-ivector-dir $ivector_dir/ivectors_${data_set}${ivector_affix} \
-     $graph ${data}_hires ${decode_dir}
-fi
-exit 0
+exit 0;
