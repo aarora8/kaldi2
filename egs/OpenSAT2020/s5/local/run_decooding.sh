491555ab5969b6d4bcb7eca234e9c7f1b9df8908
aarora8 <aarora8@jhu.edu>
2020-10-22 22:15:39 -0400

    minor fix in run_decoding

--- egs/OpenSAT2020/s5/local/run_decooding.sh
+++ egs/OpenSAT2020/s5/local/run_decooding.sh
@@ -1,22 +1,21 @@
 #!/usr/bin/env bash
+# This script is used for performing single stage decoding
+# for the development data for which we have the transcripts
+
 stage=0
 cmd=queue.pl
 . ./utils/parse_options.sh
 . ./cmd.sh
 . ./path.sh
 set -e -o pipefail
-set -o nounset                              # Treat unset variables as an error
+set -o nounset
 
-dataset=safe_t_dev1_segmented
-dir=exp/ihm/chain_1a/tdnn_b_bigger_2_aug
+dataset=safe_t_dev1
+dir=exp/chain_all/tdnn_all
+extractor=exp/nnet3_all
 nj=$(cat data/${dataset}/spk2utt | wc -l)
+
 if [ $stage -le 0 ] ; then
-  #cat data/$dataset/wav.scp | awk '{print $1 " " $1}' > data/$dataset/utt2spk
-  #utils/fix_data_dir.sh data/$dataset
-  #steps/make_mfcc.sh --cmd "$train_cmd" --nj 20 data/$dataset
-  #steps/compute_cmvn_stats.sh data/$dataset
-  #utils/fix_data_dir.sh data/${dataset}
-  nj=$(cat data/${dataset}/spk2utt | wc -l)
   utils/copy_data_dir.sh data/$dataset data/${dataset}_hires
   steps/make_mfcc.sh --nj $nj --mfcc-config conf/mfcc_hires.conf \
     --cmd "$train_cmd" data/${dataset}_hires
@@ -24,15 +23,15 @@ if [ $stage -le 0 ] ; then
   utils/fix_data_dir.sh data/${dataset}_hires
 
   steps/online/nnet2/extract_ivectors_online.sh --cmd "$train_cmd --max-jobs-run 64" --nj $nj \
-    data/${dataset}_hires exp/ihm/nnet3_1b/extractor \
-    exp/ihm/nnet3_1b/ivectors_${dataset}_hires
+    data/${dataset}_hires $extractor/extractor \
+    $extractor/ivectors_${dataset}_hires
 fi
 
 if [ $stage -le 1 ] ; then
   # decode
   steps/nnet3/decode.sh --num-threads 4 --nj $nj --cmd "$decode_cmd --max-jobs-run 64" \
     --acwt 1.0 --post-decode-acwt 10.0 \
-    --online-ivector-dir exp/ihm/nnet3_1b/ivectors_${dataset}_hires \
+    --online-ivector-dir $extractor/ivectors_${dataset}_hires \
     $dir/graph_3 data/${dataset}_hires $dir/decode_${dataset} || exit 1;
 fi
 echo "Done"
