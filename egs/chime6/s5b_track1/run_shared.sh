14d35c0e973cd0c88efb6136b492ad39e167c6f9
aarora8 <aarora8@jhu.edu>
2020-11-18 23:59:54 -0500

    adding shared changes for chime6

--- egs/chime6/s5b_track1/run_shared.sh
+++ egs/chime6/s5b_track1/run_shared.sh
@@ -40,47 +40,107 @@ chime6_corpus=${PWD}/CHiME6
 json_dir=${chime6_corpus}/transcriptions
 audio_dir=${chime6_corpus}/audio
 enhanced_dir=enhanced
-if [[ ${enhancement} == *gss* ]]; then
-  enhanced_dir=${enhanced_dir}_multiarray
-  enhancement=${enhancement}
-fi
+enhanced_dir=${enhanced_dir}_multiarray
+enhancement=${enhancement}
 
 test_sets="eval_${enhancement}"
 train_set=train_worn_simu_u400k
 
-# This script also needs the phonetisaurus g2p, srilm, beamformit
-./local/check_tools.sh || exit 1
-
-###########################################################################
-# We first generate the synchronized audio files across arrays and
-# corresponding JSON files. Note that this requires sox v14.4.2,
-# which is installed via miniconda in ./local/check_tools.sh
-###########################################################################
-
-#if [ $stage -le 0 ]; then
-#  local/generate_chime6_data.sh \
-#    --cmd "$train_cmd" \
-#    ${chime5_corpus} \
-#    ${chime6_corpus}
-#fi
-
-###########################################################################
-# We prepare dict and lang in stages 1 to 3.
-###########################################################################
-
 if [ $stage -le 1 ]; then
   echo "$0:  prepare data..."
-  for mictype in worn; do
-    local/prepare_data.sh --mictype ${mictype} \
-			  ${audio_dir}/train ${json_dir}/train data/train_${mictype}
-  done
-  for dataset in dev; do
+  for dataset in train dev; do
     for mictype in worn; do
       local/prepare_data.sh --mictype ${mictype} \
 			    ${audio_dir}/${dataset} ${json_dir}/${dataset} \
 			    data/${dataset}_${mictype}
     done
   done
+fi
+
+if [ $stage -le 2 ]; then
+  utils/copy_data_dir.sh data/train_worn data/train_worn_org
+  grep -v -e "^P11_S03" -e "^P52_S19" -e "^P53_S24" -e "^P54_S24" data/train_worn_org/text > data/train_worn/text
+  utils/fix_data_dir.sh data/train_worn
+fi
+
+if [ $stage -le 3 ]; then
+  local/prepare_data.sh --mictype gss ${enhanced_dir}/audio/train ${json_dir}/train data/train_${enhancement}
+fi
+
+if [ $stage -le 4 ]; then
+  utils/combine_data.sh data/${train_set} data/train_worn data/train_gss
+  for dset in train dev; do
+    utils/copy_data_dir.sh data/${dset}_worn data/${dset}_worn_stereo
+    grep "\.L-" data/${dset}_worn_stereo/text > data/${dset}_worn/text
+    utils/fix_data_dir.sh data/${dset}_worn
+  done
+fi
+
+if [ $stage -le 5 ]; then
+  echo "$0:  train lm ..."
+  local/prepare_dict.sh data/local/dict_nosp
+
+  utils/prepare_lang.sh \
+    data/local/dict_nosp "<unk>" data/local/lang_nosp data/lang_nosp
+
+  local/train_lms_srilm.sh \
+    --train-text data/train_worn/text --dev-text data/dev_worn/text \
+    --oov-symbol "<unk>" --words-file data/lang_nosp/words.txt \
+    data/ data/srilm
+fi
+
+if [ $stage -le 6 ]; then
+  for dset in ${train_set}; do
+    utils/copy_data_dir.sh data/${dset} data/${dset}_nosplit
+    utils/data/modify_speaker_info.sh --seconds-per-spk-max 180 data/${dset}_nosplit data/${dset}
+  done
+fi
+exit
+if [ $stage -le 7 ]; then
+  local/icsi_run_prepare_shared.sh
+  local/icsi_ihm_data_prep.sh $ICSI_DIR
+  local/icsi_ihm_scoring_data_prep.sh $ICSI_DIR dev
+  local/icsi_ihm_scoring_data_prep.sh $ICSI_DIR eval
+fi
+
+if [ $stage -le 8 ]; then
+  local/ami_text_prep.sh data/local/download
+  local/ami_ihm_data_prep.sh $AMI_DIR
+  local/ami_ihm_scoring_data_prep.sh $AMI_DIR dev
+  local/ami_ihm_scoring_data_prep.sh $AMI_DIR eval
+fi
+
+if [ $stage -le 9 ]; then
+  for dset in train dev eval; do
+    utils/data/modify_speaker_info.sh --seconds-per-spk-max 30 \
+      data/AMI/${dset}_orig data/AMI/$dset
+    utils/data/modify_speaker_info.sh --seconds-per-spk-max 30 \
+      data/ICSI/${dset}_orig data/ICSI/$dset
+  done
+fi
+
+if [ $stage -le 10 ]; then
+  utils/data/get_reco2dur.sh data/AMI/train
+  utils/data/get_reco2dur.sh data/AMI/train
+
+  utils/data/get_utt2dur.sh data/ICSI/train
+  utils/data/get_utt2dur.sh data/ICSI/train
+
+  for dataset in AMI ICSI; do
+    for split in train dev eval; do
+      cat data/$dataset/$split/text | awk '{printf $1""FS;for(i=2; i<=NF; ++i) printf "%s",tolower($i)""FS; print""}'  > data/$dataset/$split/texttmp
+      mv data/$dataset/$split/text data/$dataset/$split/textupper
+      mv data/$dataset/$split/texttmp data/$dataset/$split/text
+    done
+  done
+fi
+
+if [ $stage -le 11 ] ; then
+  local/safet_train_lms_srilm.sh \
+    --train_text data/train_safet/text --dev_text data/safe_t_dev1/text  \
+    data/ data/local/srilm
+  utils/format_lm.sh  data/lang_nosp/ data/local/srilm/lm.gz\
+    data/local/lexicon.txt  data/lang_nosp_test
 fi
 
 if [ $stage -le 2 ]; then
@@ -96,73 +156,18 @@ if [ $stage -le 2 ]; then
     data/ data/srilm
 fi
 
-#########################################################################################
-# In stages 3 to 8, we augment and fix train data for our training purpose. point source
-# noises are extracted from chime corpus. Here we use 400k utterances from array microphones,
-# its augmentation and all the worn set utterances in train.
-#########################################################################################
-
-if [ $stage -le 3 ]; then
-  # remove possibly bad sessions (P11_S03, P52_S19, P53_S24, P54_S24)
-  # see http://spandh.dcs.shef.ac.uk/chime_challenge/data.html for more details
-  utils/copy_data_dir.sh data/train_worn data/train_worn_org # back up
-  grep -v -e "^P11_S03" -e "^P52_S19" -e "^P53_S24" -e "^P54_S24" data/train_worn_org/text > data/train_worn/text
-  utils/fix_data_dir.sh data/train_worn
+if [ $stage -le 12 ] ; then
+  utils/data/combine_data.sh data/train_all data/train_safet data/AMI/train data/ICSI/train
 fi
 
-if [ $stage -le 4 ] && [[ ${enhancement} == *gss* ]]; then
-  echo "$0:  enhance data..."
-  if [ ! -d pb_chime5/ ]; then
-    local/install_pb_chime5.sh
-  fi
-
-  if [ ! -f pb_chime5/cache/chime6.json ]; then
-    (
-    cd pb_chime5
-    miniconda_dir=$HOME/miniconda3/
-    export PATH=$miniconda_dir/bin:$PATH
-    export CHIME6_DIR=$chime6_corpus
-    make cache/chime6.json
-    )
-  fi
-
-#  for dset in train; do
-#    local/run_gss.sh \
-#      --cmd "$train_cmd --max-jobs-run $gss_nj" --nj 160 \
-#      ${dset} \
-#      ${enhanced_dir} \
-#      ${enhanced_dir} || exit 1
-#  done
-
-  local/prepare_data.sh --mictype gss ${enhanced_dir}/audio/train ${json_dir}/train data/train_${enhancement}
+# Feature extraction,
+if [ $stage -le 13 ]; then
+  steps/make_mfcc.sh --nj 75 --cmd "$train_cmd" data/train_all
+  steps/compute_cmvn_stats.sh data/train_all
+  utils/fix_data_dir.sh data/train_all
 fi
 
-if [ $stage -le 5 ]; then
-  utils/combine_data.sh data/${train_set} data/train_worn data/train_gss
-
-  # only use left channel for worn mic recognition
-  # you can use both left and right channels for training
-  for dset in train dev; do
-    utils/copy_data_dir.sh data/${dset}_worn data/${dset}_worn_stereo
-    grep "\.L-" data/${dset}_worn_stereo/text > data/${dset}_worn/text
-    utils/fix_data_dir.sh data/${dset}_worn
-  done
-fi
-
-if [ $stage -le 6 ]; then
-  # Split speakers up into 3-minute chunks.  This doesn't hurt adaptation, and
-  # lets us use more jobs for decoding etc.
-  for dset in ${train_set}; do
-    utils/copy_data_dir.sh data/${dset} data/${dset}_nosplit
-    utils/data/modify_speaker_info.sh --seconds-per-spk-max 180 data/${dset}_nosplit data/${dset}
-  done
-fi
-
-##################################################################################
-# Now make 13-dim MFCC features. We use 13-dim fetures for GMM-HMM systems.
-##################################################################################
-
-if [ $stage -le 7 ]; then
+if [ $stage -le 14 ]; then
   # Now make MFCC features.
   # mfccdir should be some place with a largish disk where you
   # want to store MFCC features.
