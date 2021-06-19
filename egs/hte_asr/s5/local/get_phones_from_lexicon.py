2ef52f80283bff131aa5d72c508b32ae635158d0
aarora8 <ashish.arora.88888@gmail.com>
2021-06-19 16:24:15 -0400

    minor fix

--- egs/hte_asr/s5/local/get_phones_from_lexicon.py
+++ egs/hte_asr/s5/local/get_phones_from_lexicon.py
@@ -7,20 +7,28 @@ parser.add_argument('lexicon', type=str, help='File name of a file that contains
                     'lexicon with monophones. Each line must be: <word> <phone1> <phone2> ...')
 parser.add_argument('output_nonsilence_phones', type=str, help='Output file that contains'
                     'non-silence phones')
-
+parser.add_argument('output_lexicon', type=str, help='Output file that contains'
+                    'non-silence phones')
 def main():
 
     args = parser.parse_args()
     phonesdict = dict()
     output_nonsilphones_handle = open(args.output_nonsilence_phones, 'w', encoding='utf8')
+    output_lexicon_handle = open(args.output_lexicon, 'w', encoding='utf8')
     lexicon_handle = open(args.lexicon, 'r', encoding='utf8')
     lexicon_data = lexicon_handle.read().strip().split("\n")
     for line in lexicon_data:
         parts = line.strip().split()
+        word = parts[0]
+        if '(2)' in word or '(3)' in word or '(4)' in word or '(5)' in word:
+            word = word[:-3]
+            output_lexicon_handle.write(word + ' ' + ' '.join(parts[1:]) + '\n')
+        else:
+            output_lexicon_handle.write(word + ' ' + ' '.join(parts[1:]) + '\n')
         for phone in parts[1:]:
             phonesdict[phone] = phone
-    for phone in phonesdict:
-        if SIL iin phone:
+    for phone in sorted(phonesdict):
+        if 'SIL' in phone:
             continue
         output_nonsilphones_handle.write(phone + '\n')
 
--- egs/hte_asr/s5/local/prepare_dict.sh
+++ egs/hte_asr/s5/local/prepare_dict.sh
@@ -4,17 +4,16 @@ dst_dir=data/local/dict_nosp
 silence_phones=$dst_dir/silence_phones.txt
 optional_silence=$dst_dir/optional_silence.txt
 nonsil_phones=$dst_dir/nonsilence_phones.txt
-lexicon_raw_nosil=$dst_dir/lexicon/lexicon_raw_nosil.txt
 
 mkdir -p $dst_dir
 echo "Preparing phone lists"
 echo SIL > $silence_phones
 echo SIL > $optional_silence
 
-local/get_phones_from_lexicon.py data/lexicon.txt $nonsil_phones
+local/get_phones_from_lexicon.py data/lexicon.txt $nonsil_phones data/lexicon2.txt
 
 (echo '!SIL SIL'; echo '<UNK> SIL'; ) |\
-cat - data/lexicon.txt | sort | uniq >$dst_dir/lexicon.txt
+cat - data/lexicon2.txt | sort | uniq >$dst_dir/lexicon.txt
 echo "Lexicon text file saved as: $dst_dir/lexicon.txt"
 
 exit 0
