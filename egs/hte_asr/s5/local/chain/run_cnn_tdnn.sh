f20f6b214a8a1c3fb0986053bbd511b5ea41f5a7
aarora8 <ashish.arora.88888@gmail.com>
2021-06-19 20:19:15 -0400

    adding run cnn tdnn script

--- egs/hte_asr/s5/local/chain/run_cnn_tdnn.sh
+++ egs/hte_asr/s5/local/chain/run_cnn_tdnn.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
 
-# %WER 14.74 [ 2875 / 19507, 217 ins, 1237 del, 1421 sub ] exp/chain_a/cnn_tdnn_1a/decode_safe_t_dev1/wer_8_0.0
-# %WER 13.36 [ 2606 / 19507, 217 ins, 1110 del, 1279 sub ] exp/chain_a/cnn_tdnn_1a_spec/decode_safe_t_dev1/wer_8_0.0
-# System                      tdnn_1a_spec
-# WER                             13.36
-# Final train prob              -0.0706
-# Final valid prob              -0.0965
-# Parameters                      6.95M
-
 set -e -o pipefail
 stage=0
 nj=60
@@ -20,8 +12,8 @@ num_epochs=10
 train_stage=-10
 xent_regularize=0.1
 get_egs_stage=-10
-tree_affix=_mono
-tdnn_affix=_1b_3
+tree_affix=_1a
+tdnn_affix=_1a
 nnet3_affix=_1a
 common_egs_dir= 
 dropout_schedule='0,0@0.20,0.5@0.50,0'
@@ -42,12 +34,12 @@ where "nvcc" is installed.
 EOF
 fi
 
-#local/nnet3/run_ivector_common.sh --stage $stage \
-#                                  --nj $nj \
-#                                  --train-set $train_set \
-#                                  --gmm $gmm \
-#                                  --nnet3-affix "$nnet3_affix"
-#
+local/nnet3/run_ivector_common.sh --stage $stage \
+                                  --nj $nj \
+                                  --train-set $train_set \
+                                  --gmm $gmm \
+                                  --nnet3-affix "$nnet3_affix"
+
 gmm_dir=exp/${gmm}_${train_set}
 ali_dir=exp/${gmm}_${train_set}_ali_sp
 lores_train_data_dir=data/${train_set}_sp
@@ -59,45 +51,45 @@ dir=exp/chain${nnet3_affix}/cnn_tdnn${tdnn_affix}
 train_ivector_dir=exp/nnet3${nnet3_affix}/ivectors_${train_set}_sp_hires
 
 for f in $gmm_dir/final.mdl $lores_train_data_dir/feats.scp \
-   $train_data_dir/feats.scp; do
+   $train_data_dir/feats.scp $train_ivector_dir/ivector_online.scp; do
   [ ! -f $f ] && echo "$0: expected file $f to exist" && exit 1
 done
 
 
-#if [ $stage -le 11 ]; then
-#  nj=$(cat $ali_dir/num_jobs) || exit 1;
-#  steps/align_fmllr_lats.sh --nj $nj --cmd "$train_cmd" $lores_train_data_dir \
-#    $lang_dir $gmm_dir $lat_dir
-#  rm $lat_dir/fsts.*.gz
-#fi
-#
-#if [ $stage -le 12 ]; then
-#  echo "$0: creating lang directory with one state per phone."
-#  # Create a version of the lang/ directory that has one state per phone in the
-#  # topo file. [note, it really has two states.. the first one is only repeated
-#  # once, the second one has zero or more repeats.]
-#  if [ -d data/lang_chain ]; then
-#    if [ data/lang_chain/L.fst -nt $lang_dir/L.fst ]; then
-#      echo "$0: data/lang_chain already exists, not overwriting it; continuing"
-#    else
-#      echo "$0: data/lang_chain already exists and seems to be older than data/lang..."
-#      echo " ... not sure what to do.  Exiting."
-#      exit 1;
-#    fi
-#  else
-#    cp -r $lang_dir data/lang_chain
-#    silphonelist=$(cat data/lang_chain/phones/silence.csl) || exit 1;
-#    nonsilphonelist=$(cat data/lang_chain/phones/nonsilence.csl) || exit 1;
-#    # Use our special topology... note that later on may have to tune this topology.
-#    steps/nnet3/chain/gen_topo.py $nonsilphonelist $silphonelist >data/lang_chain/topo
-#  fi
-#fi
+if [ $stage -le 11 ]; then
+  nj=$(cat $ali_dir/num_jobs) || exit 1;
+  steps/align_fmllr_lats.sh --nj $nj --cmd "$train_cmd" $lores_train_data_dir \
+    $lang_dir $gmm_dir $lat_dir
+  rm $lat_dir/fsts.*.gz
+fi
 
-#if [ $stage -le 14 ]; then
-#  steps/nnet3/chain/build_tree.sh --frame-subsampling-factor 3 \
-#      --context-opts "--context-width=1 --central-position=0" \
-#      --cmd "$train_cmd" 42 ${lores_train_data_dir} data/lang_chain $ali_dir $tree_dir
-#fi
+if [ $stage -le 12 ]; then
+  echo "$0: creating lang directory with one state per phone."
+  # Create a version of the lang/ directory that has one state per phone in the
+  # topo file. [note, it really has two states.. the first one is only repeated
+  # once, the second one has zero or more repeats.]
+  if [ -d data/lang_chain ]; then
+    if [ data/lang_chain/L.fst -nt $lang_dir/L.fst ]; then
+      echo "$0: data/lang_chain already exists, not overwriting it; continuing"
+    else
+      echo "$0: data/lang_chain already exists and seems to be older than data/lang..."
+      echo " ... not sure what to do.  Exiting."
+      exit 1;
+    fi
+  else
+    cp -r $lang_dir data/lang_chain
+    silphonelist=$(cat data/lang_chain/phones/silence.csl) || exit 1;
+    nonsilphonelist=$(cat data/lang_chain/phones/nonsilence.csl) || exit 1;
+    # Use our special topology... note that later on may have to tune this topology.
+    steps/nnet3/chain/gen_topo.py $nonsilphonelist $silphonelist >data/lang_chain/topo
+  fi
+fi
+
+if [ $stage -le 14 ]; then
+  steps/nnet3/chain/build_tree.sh --frame-subsampling-factor 3 \
+      --context-opts "--context-width=2 --central-position=1" \
+      --cmd "$train_cmd" 5000 ${lores_train_data_dir} data/lang_chain $ali_dir $tree_dir
+fi
 
 if [ $stage -le 15 ]; then
   mkdir -p $dir
@@ -117,15 +109,22 @@ if [ $stage -le 15 ]; then
 
   mkdir -p $dir/configs
   cat <<EOF > $dir/configs/network.xconfig
+  input dim=100 name=ivector
   input dim=40 name=input
   idct-layer name=idct input=input dim=40 cepstral-lifter=22 affine-transform-file=$dir/configs/idct.mat
+  linear-component name=ivector-linear $ivector_affine_opts dim=200 input=ReplaceIndex(ivector, t, 0)
+  batchnorm-component name=ivector-batchnorm target-rms=0.025
+  batchnorm-component name=idct-batchnorm input=idct
   spec-augment-layer name=idct-spec-augment freq-max-proportion=0.5 time-zeroed-proportion=0.2 time-mask-max-frames=20
+  combine-feature-maps-layer name=combine_inputs input=Append(idct-spec-augment, ivector-batchnorm) num-filters1=1 num-filters2=5 height=40
   conv-relu-batchnorm-layer name=cnn1 $cnn_opts height-in=40 height-out=40 time-offsets=-1,0,1 height-offsets=-1,0,1 num-filters-out=48 learning-rate-factor=0.333 max-change=0.25
   conv-relu-batchnorm-layer name=cnn2 $cnn_opts height-in=40 height-out=40 time-offsets=-1,0,1 height-offsets=-1,0,1 num-filters-out=48
   conv-relu-batchnorm-layer name=cnn3 $cnn_opts height-in=40 height-out=20 height-subsample-out=2 time-offsets=-1,0,1 height-offsets=-1,0,1 num-filters-out=64
   conv-relu-batchnorm-layer name=cnn4 $cnn_opts height-in=20 height-out=20 time-offsets=-1,0,1 height-offsets=-1,0,1 num-filters-out=64
   conv-relu-batchnorm-layer name=cnn5 $cnn_opts height-in=20 height-out=10 height-subsample-out=2 time-offsets=-1,0,1 height-offsets=-1,0,1 num-filters-out=64
   conv-relu-batchnorm-layer name=cnn6 $cnn_opts height-in=10 height-out=5 height-subsample-out=2 time-offsets=-1,0,1 height-offsets=-1,0,1 num-filters-out=128
+  # the first TDNN-F layer has no bypass (since dims don't match), and a larger bottleneck so the
+  # information bottleneck doesn't become a problem.
   tdnnf-layer name=tdnnf7 $tdnnf_first_opts dim=1024 bottleneck-dim=256 time-stride=0
   tdnnf-layer name=tdnnf8 $tdnnf_opts dim=1024 bottleneck-dim=128 time-stride=3
   tdnnf-layer name=tdnnf9 $tdnnf_opts dim=1024 bottleneck-dim=128 time-stride=3
@@ -138,6 +137,8 @@ if [ $stage -le 15 ]; then
   linear-component name=prefinal-l dim=192 $linear_opts
   prefinal-layer name=prefinal-chain input=prefinal-l $prefinal_opts big-dim=1024 small-dim=192
   output-layer name=output include-log-softmax=false dim=$num_targets $output_opts
+  prefinal-layer name=prefinal-xent input=prefinal-l $prefinal_opts big-dim=1024 small-dim=192
+  output-layer name=output-xent dim=$num_targets learning-rate-factor=$learning_rate_factor $output_opts
 EOF
   steps/nnet3/xconfig_to_configs.py --xconfig-file $dir/configs/network.xconfig --config-dir $dir/configs/
 fi
@@ -146,12 +147,14 @@ fi
 if [ $stage -le 16 ]; then
   if [[ $(hostname -f) == *.clsp.jhu.edu ]] && [ ! -d $dir/egs/storage ]; then
     utils/create_split_dir.pl \
-     /export/b0{3,4,5,6}/$USER/kaldi-data/egs/opensat-$(date +'%m_%d_%H_%M')/s5/$dir/egs/storage $dir/egs/storage
+     /export/b0{3,4,5,6}/$USER/kaldi-data/egs/hte-$(date +'%m_%d_%H_%M')/s5/$dir/egs/storage $dir/egs/storage
   fi
 
   steps/nnet3/chain/train.py --stage=$train_stage \
     --cmd="$gpu_cmd" \
+    --feat.online-ivector-dir=$train_ivector_dir \
     --feat.cmvn-opts="--norm-means=false --norm-vars=false" \
+    --chain.xent-regularize $xent_regularize \
     --chain.leaky-hmm-coefficient=0.1 \
     --chain.l2-regularize=0.0 \
     --chain.apply-deriv-weights=false \
@@ -168,7 +171,7 @@ if [ $stage -le 16 ]; then
     --trainer.optimization.momentum=0.0 \
     --egs.chunk-width=$chunk_width \
     --egs.dir="$common_egs_dir" \
-    --egs.opts="--frames-overlap-per-eg 0 --constrained false" \
+    --egs.opts="--frames-overlap-per-eg 0" \
     --cleanup.remove-egs=$remove_egs \
     --use-gpu=true \
     --reporting.email="$reporting_email" \
@@ -178,12 +181,20 @@ if [ $stage -le 16 ]; then
     --dir=$dir  || exit 1;
 fi
 
-if [ $stage -le 18 ]; then
-  utils/mkgraph.sh --self-loop-scale 1.0 data/lang_nosp_test $dir $dir/graph
+if [ $stage -le 17 ]; then
 
-  steps/nnet3/decode.sh --nj 20 --cmd "$decode_cmd" \
-      --acwt 1.0 --post-decode-acwt 10.0 \
-     $dir/graph data/safe_t_dev1_hires $dir/decode_safe_t_dev1 || exit 1;
+  steps/online/nnet2/extract_ivectors_online.sh --cmd "$train_cmd" --nj 20 \
+    data/dev_hires exp/nnet3${nnet3_affix}/extractor \
+    exp/nnet3${nnet3_affix}/ivectors_dev_hires
+
+  utils/mkgraph.sh --self-loop-scale 1.0 data/lang_nosp_test $dir $dir/graph
+fi
+
+if [ $stage -le 18 ]; then
+    steps/nnet3/decode.sh --nj 20 --cmd "$decode_cmd" \
+        --acwt 1.0 --post-decode-acwt 10.0 \
+        --online-ivector-dir exp/nnet3${nnet3_affix}/ivectors_dev_hires \
+       $dir/graph data/dev_hires $dir/decode_dev || exit 1;
 fi
 exit 0
 
