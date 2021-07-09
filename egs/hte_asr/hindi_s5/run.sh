8fca3bd5be47e661a9b3181f6d963c73beb8e11b
aarora8 <ashish.arora.88888@gmail.com>
2021-07-08 22:11:48 -0400

    minor fix

--- egs/hte_asr/hindi_s5/run.sh
+++ egs/hte_asr/hindi_s5/run.sh
@@ -13,7 +13,8 @@ if [ $stage -le 1 ]; then
   mkdir -p data/local
   cp -r /export/common/data/corpora/ASR/IITM_Indian_ASR_Challenge_2021/Indian_Language_Database/Hindi/dictionary/Hindi_lexicon.txt data/local/lexicon.txt
   local/prepare_data_mw.sh
-  #local/create_train_test_split.sh
+  mv data/train_Hindi_jhu data/train
+  mv data/dev_Hindi_jhu data/dev
   local/prepare_dict.sh
   utils/prepare_lang.sh data/local/dict_nosp '<UNK>' data/local/lang_nosp data/lang_nosp_test
   utils/validate_lang.pl data/lang_nosp_test
@@ -24,7 +25,7 @@ if [ $stage -le 3 ] ; then
   utils/format_lm.sh  data/lang_nosp_test data/local/lm/lm.gz \
     data/local/lexicon2.txt  data/lang_nosp_test
 fi
-
+exit
 # Feature extraction,
 if [ $stage -le 4 ]; then
   steps/make_mfcc.sh --nj 75 --cmd "$train_cmd" data/train
